0392837b93d32c9dff3144caae572d9e09298128
JordBant <jaybant23@gmail.com>
2022-08-16 23:07:47 -0400

    Configure media queries on dropdown for desktop

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import CallToAction from './components/call-to-action/cta-component';
 import HeroImg from './components/icon-components/hero-img/hero-image-component';
 import NavMenu from './components/navigation-menu/navigation-menu';
 
+
 function App() {
   return (
-    <div className="App">
+    <main className="App">
       <NavMenu />
       <div className="main-container">
         <HeroImg/>
         <CallToAction/>
       </div>
-    </div>
+    </main>
   );
 }
 
